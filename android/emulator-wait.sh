62e3a8a5c40951d881417db330f07a742f5dac37
Tony Li <tonyli.lives@gmail.com>
2014-03-18 17:48:46 -0700

    updated sdk for travis

---
+++ android/emulator-wait.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# https://github.com/embarkmobile/android-maven-example/blob/master/wait_for_emulator
+
+bootanim=""
+failcounter=0
+until [[ "$bootanim" =~ "stopped" ]]; do
+    bootanim=`adb -e shell getprop init.svc.bootanim 2>&1`
+    echo "$bootanim"
+    if [[ "$bootanim" =~ "not found" ]]; then
+        let "failcounter += 1"
+        if [[ $failcounter -gt 3 ]]; then
+            echo "Failed to start emulator"
+            exit 1
+        fi
+    fi
+    sleep 1
+done
+echo "Done"
