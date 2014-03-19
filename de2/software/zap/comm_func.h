e24125fcb82d705f1c282ca4c100e5016d6fc753
scolin22 <colin@bunnylaundering.com>
2014-03-18 22:28:09 -0700

    halfway, will integrate players info

---
+++ de2/software/zap/comm_func.h
@@ -0,0 +1,15 @@
+#ifndef COMM_FUNC_H_
+#define COMM_FUNC_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "altera_up_avalon_rs232.h"
+#include <string.h>
+
+void tell_user_pid_role(int pid, int role);
+
+void tell_user_all_opponent_range_role(int pid, *pid_range_role_array)
+
+
+
+#endif
