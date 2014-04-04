e764ee1c5330098cdece7833bd3491b83c931305
Amitoj Kooner <kooner47@gmail.com>
2014-04-04 14:48:02 -0700

    Add helper boolean methods for determining card targets

--- android/src/com/zap/main/Card.java
+++ android/src/com/zap/main/Card.java
@@ -1,46 +1,134 @@
 package com.zap.main;
 
 public class Card {
-	public final int cid;
-	public final String name;
-	public final char border;
-	public final char number;
-	public final char suit;
-	public final boolean zap;
-	public final boolean missed;
-	public final int life;
-	public final boolean forceDiscard;
-	public final int draw;
-	public final boolean onePlayer;
-	public final boolean allPlayers;
-	public final boolean onePlayerReachable;
-	public final int onePlayerFixed;
-	//space duel, indians, general store
-	//space jail, dynamite, barrel, scope, mustang
-	Card(int cid, String name, char border, char number, char suit, boolean zap, 
-			boolean missed, int life, boolean forceDiscard, int draw, boolean onePlayer, 
-			boolean allPlayers, boolean onePlayerReachable, int onePlayerFixed) {
-		this.cid = cid;
-		this.name = name;
-		this.border = border;
-		this.number = number;
-		this.suit = suit;
-		this.zap = zap;
-		this.missed = missed;
-		this.life = life;
-		this.forceDiscard = forceDiscard;
-		this.draw = draw;
-		this.onePlayer = onePlayer;
-		this.allPlayers = allPlayers;
-		this.onePlayerReachable = onePlayerReachable;
-		this.onePlayerFixed = onePlayerFixed;
-	}	
-	
-	public boolean isGunCard() {
-		if (border == 'L' && onePlayerFixed != 0) {
-			return true;
-		} else {
-			return false;
+    public final int cid;
+    public final String name;
+    public final char border;
+    public final char number;
+    public final char suit;
+    public final boolean zap;
+    public final boolean missed;
+    public final int life;
+    public final boolean forceDiscard;
+    public final int draw;
+    public final boolean onePlayer;
+    public final boolean allPlayers;
+    public final boolean onePlayerReachable;
+    public final int onePlayerFixed;
+
+    // space duel, indians, general store
+    // space jail, dynamite, barrel, scope, mustang
+    Card(int cid, String name, char border, char number, char suit,
+            boolean zap, boolean missed, int life, boolean forceDiscard,
+            int draw, boolean onePlayer, boolean allPlayers,
+            boolean onePlayerReachable, int onePlayerFixed) {
+        this.cid = cid;
+        this.name = name;
+        this.border = border;
+        this.number = number;
+        this.suit = suit;
+        this.zap = zap;
+        this.missed = missed;
+        this.life = life;
+        this.forceDiscard = forceDiscard;
+        this.draw = draw;
+        this.onePlayer = onePlayer;
+        this.allPlayers = allPlayers;
+        this.onePlayerReachable = onePlayerReachable;
+        this.onePlayerFixed = onePlayerFixed;
+    }
+
+    public boolean isGunCard() {
+        if (border == 'L' && onePlayerFixed != 0) {
+            return true;
+        } else {
+            return false;
         }
+    }
+
+    public boolean selfTarget() {
+        return isGunCard() || isMissed() || isBeer() || isStageCoach()
+                || isWellsFargo() || isBarrel() || isMustang() || isScope()
+                || isDynamite();
+    }
+
+    public boolean allPlayersIncSelf() {
+        return isSaloon() || isGeneralStore();
+    }
+
+    public boolean allPlayersNotSelf() {
+        return isGatling() || isAliens();
+    }
+
+    public boolean onePlayerNotSelf() {
+        return isBang() || isPanic() || isCatBalou() || isDuel() || isJail();
+    }
+
+    public boolean isBang() {
+        return name.equals("Zap!");
+    }
+
+    public boolean isDuel() {
+        return name.equals("Duel");
+    }
+
+    public boolean isAliens() {
+        return name.equals("Aliens");
+    }
+
+    public boolean isGeneralStore() {
+        return name.equals("General Store");
+    }
+
+    public boolean isMissed() {
+        return name.equals("Missed");
+    }
+
+    public boolean isBeer() {
+        return name.equals("Beer");
+    }
+
+    public boolean isStageCoach() {
+        return name.equals("Stagecoach");
+    }
+
+    public boolean isWellsFargo() {
+        return name.equals("Wells Fargo");
+    }
+
+    public boolean isBarrel() {
+        return name.equals("Barrel");
+    }
+
+    public boolean isMustang() {
+        return name.equals("Mustang");
+    }
+
+    public boolean isScope() {
+        return name.equals("Scope");
+    }
+
+    public boolean isSaloon() {
+        return name.equals("Saloon");
+    }
+
+    public boolean isDynamite() {
+        return name.equals("Dynamite");
+    }
+
+    public boolean isGatling() {
+        return name.equals("Gatling");
+    }
+
+    public boolean isPanic() {
+        return name.equals("Panic");
+    }
+
+    public boolean isCatBalou() {
+        return name.equals("Cat Balou");
+    }
+
+    public boolean isJail() {
+        return name.equals("Space Jail");
     }
 }
